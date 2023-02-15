82864a55a27a6bf63b0569ace47e6df0ada24ae1
favour <femmaje@gmail.com>
2023-02-15 10:07:24 -0800

    latest

--- src/E-Hub/components/Animations/AnimatedText.jsx
+++ src/E-Hub/components/Animations/AnimatedText.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import MovingText from "react-moving-text";
+import styled from "styled-components";
+
+const TextDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2px;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    & span{
+      text-align: center;
+    }
+  }
+`;
 
 const AnimatedText = () => {
   return (
     <>
-      First Class blend of Entrepreneurial Media, WorkSpace, Enducation and
-      Enterprise.
+      <TextDiv>
+        <span>
+          First Class blend of Entrepreneurial
+        </span>
+        <MovingText
+          type="typewriter"
+          dataText={["Media", "WorkSpace", "Enducation", "Enterprise"]}
+        />
+      </TextDiv>
     </>
   );
 };
